a1a0aec683238d8d28b40b6584c92c03ac0a2e13
Hema Chandra <132965396+hemachandra-807@users.noreply.github.com>
2025-04-04 09:36:23 +0530

    Update PrimeNumberByUserInput.py

--- Python Programs 03-April-2025/PrimeNumberByUserInput.py
+++ Python Programs 03-April-2025/PrimeNumberByUserInput.py
@@ -2,7 +2,7 @@ def is_Prime(num):
     if num <= 1:
         return "Not a Prime Number"
     else:
-        for i in range(2, num):
+        for i in range(2, int(num ** 0.5):
             if num % i == 0:
                 return "Not a Prime Number"
     return "Prime Number"
