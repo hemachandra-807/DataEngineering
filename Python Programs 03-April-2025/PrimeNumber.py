884b397c2b1aede3c4136059058f7d4b943c3a56
Hema Chandra <132965396+hemachandra-807@users.noreply.github.com>
2025-04-04 09:35:42 +0530

    Update PrimeNumber.py

--- Python Programs 03-April-2025/PrimeNumber.py
+++ Python Programs 03-April-2025/PrimeNumber.py
@@ -3,7 +3,7 @@ def is_Prime(num):
     if num <=1: # other use the condition like (num < 2)
         return False
     else:
-        for i in range(2, num):
+        for i in range(2, int(num ** 0.5):
             if num % i == 0:
                 return False
     return True
